2c68b7308ffcead78566e87572f7904c818ca729
mitho0096 <mithilesh0996@gmail.com>
2022-07-19 14:23:07 +0530

    to do list

--- main.js
+++ main.js
@@ -14,8 +14,25 @@ const listLength=()=>{
 
 const createListElement=()=>{
     let li=document.createElement("li");
-
     li.appendChild(document.createTextNode(input.value))
+
+    //to add delete span
+    let dBtn=document.createElement("span");
+    dBtn.appendChild(document.createTextNode("X"))
+
+    //as before
+    li.appendChild(dBtn)
     ul.appendChild(li)
-    input.value=" ";
+    input.value="";
+
+    const deleteItem=()=>{
+        li.classList.add("delete")
+    }
+    const doneItem=()=>{
+        li.classList.add("done");
+    }
+    li.addEventListener("click",doneItem)
+
+    dBtn.addEventListener("click",deleteItem)
+
 }
